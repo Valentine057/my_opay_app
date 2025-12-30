48934b280043f5ff88e316c8a9991c9d1d7313bd
Bennet Otoho <bennet2000@outlook.com>
2025-12-30 12:22:59 +0100

    create user info service

---
+++ lib/services/user_info_service.dart
@@ -0,0 +1,16 @@
+import 'package:http/http.dart' as http;
+import '../models/user_info.dart';
+
+class UserInfoService {
+  Future<UserInfo?> getUserInfo() async {
+    var client = http.Client();
+    var uri = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    var response = await client.get(uri);
+
+    if (response.statusCode == 200) {
+      var xml = response.body;
+      return userInfoFromXml(xml);
+    }
+    return null;
+  }
+}
